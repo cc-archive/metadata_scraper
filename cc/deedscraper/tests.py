b7e3d99e862908226238ba9e14bbb5eeab78afef
nyergler <nyergler@committer.creativecommons.org>
2007-03-14 21:35:26 +0000

    Unittest suite for the metadata scraper.

--- cc/deedscraper/tests.py
+++ cc/deedscraper/tests.py
@@ -1,4 +1,4 @@
-## Copyright (c) 2006-2007 Nathan R. Yergler, Creative Commons
+## Copyright (c) 2007 Nathan R. Yergler, Creative Commons
 
 ## Permission is hereby granted, free of charge, to any person obtaining
 ## a copy of this software and associated documentation files (the "Software"),
@@ -18,23 +18,137 @@
 ## FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ## DEALINGS IN THE SOFTWARE.
 
-"""Test harness for doctests."""
+"""Unit tests for the cc.deedscraper application."""
 
 import os
 import doctest
 import unittest
 
-def test_suite():
-    """Return a unittest TestSuite created from our doctests."""
+import cherrypy
+from cherrypy.test import helper
 
-    if os.path.exists(os.path.join(os.path.dirname(__file__), 'README')):
-        README = "./README"
-    else:
-        # maybe we're in a check out and README isn't in the cwd
-        README = '../../README'
+import cc.deedscraper.app
 
-    return doctest.DocFileSuite(README)
+class DefaultValuesTest(helper.CPWebCase):
+
+    def testNoRootMethod(self):
+        """cc.deedscraper does not define an index method."""
+
+        self.getPage('/')
+        self.assertStatus(404)
+
+
+    def testNoUrlProvided(self):
+        """Calling /scrape with no parameters should return an empty result."""
+
+        self.getPage('/scrape')
+        self.assertBody('{}')
+
+class ScrapingTests(helper.CPWebCase):
+    """Tests scraping our sample static pages."""
+
+    def testNoMetadata(self):
+        """Many pages will contain no metadata, but will not throw an error."""
+
+        self.getPage('/scrape?url=http://localhost:8080/static/no_metadata.html')
+        self.assertStatus(200)
+        self.assertBody('({"morePermissionsDomain": "", '
+                        '"morePermissions": "", "attributionUrl": "", '
+                        '"attributionName": "", "licenseUrl": ""})')
+
+    def testLicenseMetadata(self):
+        """Scrape an xhtml:license assertion."""
+
+        self.getPage('/scrape?url=http://localhost:8080/static/license.html')
+        self.assertStatus(200)
+        self.assertBody('({"morePermissionsDomain": "", '
+                        '"morePermissions": "", "attributionUrl": "", '
+                        '"attributionName": "", '
+                        '"licenseUrl": "http:\/\/creativecommons.org\/licenses\/by\/3.0\/"})')
+
+    def testCcLicenseMetadata(self):
+        """Scrape a cc:license assertion (with or without namespace declaration)."""
+
+        # with namespace declaration
+        self.getPage('/scrape?url=http://localhost:8080/static/license_only1.html')
+        self.assertStatus(200)
+        self.assertBody('({"morePermissionsDomain": "", '
+                        '"morePermissions": "", "attributionUrl": "", '
+                        '"attributionName": "", '
+                        '"licenseUrl": "http:\/\/creativecommons.org\/licenses\/by\/3.0\/"})')
+
+        # without namespace declaration
+        self.getPage('/scrape?url=http://localhost:8080/static/license_only2.html')
+        self.assertStatus(200)
+        self.assertBody('({"morePermissionsDomain": "", '
+                        '"morePermissions": "", "attributionUrl": "", '
+                        '"attributionName": "", '
+                        '"licenseUrl": "http:\/\/creativecommons.org\/licenses\/by\/3.0\/"})')
+
+    def testDefaultccNamespace(self):
+        """The cc: namesapce defaults to http://web.resource.org/cc#"""
+
+        self.getPage('/scrape?url=http://localhost:8080/static/cc_license.html')
+        self.assertStatus(200)
+        self.assertBody('({"morePermissionsDomain": "magnatune.com", '
+                        '"morePermissions": "https:\/\/magnatune.com\/artists\/Solace", '
+                        '"attributionUrl": "http:\/\/localhost:8080\/artists\/solace", '
+                        '"attributionName": "Solace", '
+                        '"licenseUrl": "http:\/\/creativecommons.org\/licenses\/by\/3.0\/"})')
+
+    def testAttributionMetadata(self):
+        """Test full metdata extraction."""
+
+        self.getPage('/scrape?url=http://localhost:8080/static/attrib.html')
+        self.assertStatus(200)
+        self.assertBody('({"morePermissionsDomain": "magnatune.com", '
+                        '"morePermissions": "https:\/\/magnatune.com\/artists\/Solace", '
+                        '"attributionUrl": "http:\/\/localhost:8080\/artists\/solace", '
+                        '"attributionName": "Solace", '
+                        '"licenseUrl": "http:\/\/creativecommons.org\/licenses\/by\/3.0\/"})')
+
+    def test404(self):
+        """Errors resulting from page requests should result in am empty set."""
+
+        self.getPage('/scrape?url=http://localhost:8080/static/a404.html')
+        self.assertStatus(200)
+        self.assertBody('({"morePermissionsDomain": "", '
+                        '"morePermissions": "", "attributionUrl": "", '
+                        '"attributionName": "", '
+                        '"licenseUrl": ""})')
+
+class AttributionTest(helper.CPWebCase):
+    """Some pages will only contain attribution metadata."""
+
+    def testFoo(self):
+
+        self.getPage('/static/index.html')
+        self.assertStatus(200)
+
+def bootstrap_app():
+    """Bootstrap the application and configuration for testing."""
+
+    cherrypy.config.update({
+            'environment' : 'test_suite'
+            })
+
+    # set up static files used for testing
+    conf = {
+        '/' : {'tools.staticdir.root' : 
+               os.path.dirname(cc.deedscraper.__file__)},
+        '/static' : {'tools.staticdir.on' : True,
+                     'tools.staticdir.dir' : 'static'
+                     },
+        }
     
+    cherrypy.tree.mount(cc.deedscraper.app.DeedScraper(), config=conf)
+
+def runtests():
+    """Start the application and run the tests."""
+
+    bootstrap_app()
+    helper.testmain()
+
 if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    runner.run(test_suite())
+
+    runtests()
