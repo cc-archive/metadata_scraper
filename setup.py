962fa5d20ce61ee0f77a390857967dcfcf6cb63b
nyergler <nyergler@committer.creativecommons.org>
2008-08-13 15:36:05 +0000

    Require rdfadict >= 0.5.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-## Copyright (c) 2007 Nathan R. Yergler, Creative Commons
+## Copyright (c) 2007-2008 Nathan R. Yergler, Creative Commons
 
 ## Permission is hereby granted, free of charge, to any person obtaining
 ## a copy of this software and associated documentation files (the "Software"),
@@ -28,7 +28,7 @@ setup(
     # scripts and dependencies
     dependency_links = ['http://download.zope.org/distribution/'],
     install_requires = ['setuptools',
-                        'rdfadict[tidy]>=0.4.2',
+                        'rdfadict>=0.5',
                         'simplejson',
                         'CherryPy<=3.0.999',
 			'zdaemon',
