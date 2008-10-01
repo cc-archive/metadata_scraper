fc4be63daf4703c386cfda289871f7d91ac7d3fe
nyergler <nyergler@committer.creativecommons.org>
2008-10-01 16:49:27 +0000

    New source layout for existing deed javascript.

---
+++ javascript/src/core.js
@@ -0,0 +1,113 @@
+
+
+// ************************************************************************
+// ************************************************************************
+// ** 
+// **  Parsing/Scraping/Dispatch
+// **
+
+YAHOO.cc.license_uri = function(license_uri) {
+
+    // ensure that the license_uri is canonical
+    // note that this is a CC-ism, although this only runs in deeds @ CC,
+    // so we're fine with that
+
+    if (license_uri.charAt(license_uri.length - 1) == '/') return license_uri;
+
+    return license_uri.substring(0, license_uri.lastIndexOf('/') + 1);
+
+} // license_uri
+
+YAHOO.cc.get_license = function (metadata, subject) {
+
+    // Return the license URI for the given subject; if no license is
+    // asserted, return null.  This looks for xhtml:license, dc:license,
+    // and cc:license in that order.
+
+    if (!metadata[subject]) return null;
+
+    var license = 
+        metadata[subject]['http://www.w3.org/1999/xhtml/vocab#license'] ||
+        metadata[subject]['http://purl.org/dc/terms/license'] ||
+        metadata[subject]['http://creativecommons.org/ns#license'] || 
+        null;
+
+    if (license) return license[0];
+
+    return null;
+
+} // get_license
+
+YAHOO.cc.success = function (response) {
+
+    if (response.status != 200) return;
+
+    var referer = response.argument;
+    var license_url = YAHOO.cc.license_uri(document.URL);
+    var metadata = YAHOO.lang.JSON.parse(response.responseText);
+    var subject = null;
+
+    // see if the referrer has metadata and is licensed under this license
+    if ( (metadata.subjects.indexOf(referer) > -1) &&
+         (YAHOO.cc.get_license(metadata.triples, referer) == license_url) ) {
+
+	subject = referer;
+
+    } else {
+	
+	// no metadata about the referrer; see if we only have one
+	// subject with a license pointing at this page
+	var license_subjects = [];
+
+	for (var i = 0; i < metadata.subjects.length; i++) {
+	    if (YAHOO.cc.get_license(metadata, metadata.subjects[i]) == 
+		license_url) {
+		license_subjects.push(metadata.subjects[i]);
+	    } // if (subject, license, document.URL) is asserted
+
+	} // for each subject
+
+	// see if more than one matches
+	if (license_subjects.length == 1) {
+
+	    // only one, we can make an assertion
+	    subject = license_subjects[0];
+
+	} // if only one subject with this license
+
+    } // if the referrer is not licensed under this license
+
+    YAHOO.cc.plus.insert(metadata.triples, subject);
+
+    YAHOO.cc.attribution.add_details(metadata.triples, subject);
+    YAHOO.cc.attribution.add_copy_paste(metadata.triples, subject);
+
+} // success
+
+YAHOO.cc.failure = function () {
+
+} // failure
+
+YAHOO.cc.load = function () {
+
+    r = document.referrer;
+    if (r.match('^http://')) {
+
+	// construct the request callback
+	var callback = {
+	    success: YAHOO.cc.success,
+	    failure: YAHOO.cc.failure,
+	    argument: r
+	};
+
+	// initialize the header to include the Referer
+	YAHOO.util.Connect.initHeader('Referer', document.URL, true);
+
+	var url = '/apps/triples?url=' + encodeURIComponent(r);
+	YAHOO.util.Connect.asyncRequest('GET', url, callback, null);
+
+    } // if refered from http:// request
+
+} // load
+
+YAHOO.util.Event.onDOMReady(YAHOO.cc.load);
---
+++ javascript/src/header.js
@@ -0,0 +1,59 @@
+YAHOO.namespace("cc");
+YAHOO.namespace("cc.plus");
+YAHOO.namespace("cc.attribution");
+
+
+// ************************************************************************
+// ************************************************************************
+// ** 
+// **  support functions
+// **
+
+/*
+	parseUri 1.2.1
+	(c) 2007 Steven Levithan <stevenlevithan.com>
+	MIT License
+*/
+
+function parseUri (str) {
+	var	o   = parseUri.options,
+		m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+		uri = {},
+		i   = 14;
+
+	while (i--) uri[o.key[i]] = m[i] || "";
+
+	uri[o.q.name] = {};
+	uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+		if ($1) uri[o.q.name][$1] = $2;
+	});
+
+	return uri;
+};
+
+parseUri.options = {
+	strictMode: false,
+	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+	q:   {
+		name:   "queryKey",
+		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+	},
+	parser: {
+		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*):?([^:@]*))?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+	}
+};
+
+function addQSParameter(url, key, value) {
+    var url_nohash = url;
+    var hash = '';
+    var hash_index = (url.indexOf('#'));
+    if (hash_index != -1) {
+        url_nohash = url.substring(0, hash_index);
+        hash = url.substring(hash_index);
+    }
+    url_nohash += (url.indexOf('?') == -1) ? '?' : '&';
+    url_nohash += key + '=' + value;
+    return url_nohash + hash;
+} // addQSParameter
+
