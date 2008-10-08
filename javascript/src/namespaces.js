13c5faff6964481ecf14c05f94c623caf98c353a
nyergler <nyergler@committer.creativecommons.org>
2008-10-08 15:57:40 +0000

    Convenience functions for generating namespace-contained URIs.

---
+++ javascript/src/namespaces.js
@@ -0,0 +1,12 @@
+var SIOC = function(part) {
+    return "http://rdfs.org/sioc/ns#" + part;
+} // SIOC
+
+var POWDER = function(part) {
+    return "http://www.w3.org/2007/05/powder#" + part;
+} // POWDER
+
+var DCT = function(part) {
+    return 'http://purl.org/dc/terms/' + part;
+}
+
