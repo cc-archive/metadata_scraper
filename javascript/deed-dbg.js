66476a1b126c3414159c620494732c9c0dc440b2
nyergler <nyergler@committer.creativecommons.org>
2008-10-27 20:14:11 +0000

    Regenerated combined javascript files.

--- javascript/deed-dbg.js
+++ javascript/deed-dbg.js
@@ -372,6 +372,25 @@ YAHOO.cc.attribution.add_copy_paste = function (metadata, subject) {
 
 } // add_copy_paste
 
+/**
+ * Campaign Header
+ **/
+
+YAHOO.cc.campaign.show = function() {
+
+    // construct the text to insert
+    var network_text = 'Help Build the Commons &mdash; <a href="http://support.creativecommons.org/">Join Today!</a>';
+
+    // create the new module to display the alert
+    var module = new YAHOO.widget.Module("campaign", {visible:true});
+    module.setBody(network_text);
+    module.render(YAHOO.util.Dom.get("header"));
+    module.show();
+
+} // YAHOO.cc.campaign.show
+
+
+YAHOO.util.Event.onDOMReady(YAHOO.cc.campaign.show);
 
 
 // ************************************************************************
