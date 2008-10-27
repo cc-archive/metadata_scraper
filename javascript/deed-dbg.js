89729d34e45b70d3cdb91be9672f7b9bb7cb9592
nyergler <nyergler@committer.creativecommons.org>
2008-10-27 20:26:35 +0000

    Regenerated.

--- javascript/deed-dbg.js
+++ javascript/deed-dbg.js
@@ -376,19 +376,25 @@ YAHOO.cc.attribution.add_copy_paste = function (metadata, subject) {
  * Campaign Header
  **/
 
+YAHOO.namespace("cc.campaign");
+
 YAHOO.cc.campaign.show = function() {
+
+    var header = YAHOO.util.Dom.get("header");
 
     // construct the text to insert
     var network_text = 'Help Build the Commons &mdash; <a href="http://support.creativecommons.org/">Join Today!</a>';
 
     // create the new module to display the alert
-    var module = new YAHOO.widget.Module("campaign", {visible:true});
+    var module = new YAHOO.widget.Module("campaign", {visible:false});
     module.setBody(network_text);
-    module.render(YAHOO.util.Dom.get("header"));
+    module.render(header);
+
+    // move things around to get the order correct
+    YAHOO.util.Dom.insertBefore(module.element, YAHOO.util.Dom.getFirstChild(header));
     module.show();
 
 } // YAHOO.cc.campaign.show
-
 
 YAHOO.util.Event.onDOMReady(YAHOO.cc.campaign.show);
 
