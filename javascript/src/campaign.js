000af698656f95e10d21c8dc10a93d0e3080ed98
bse <bse@committer.creativecommons.org>
2008-11-12 01:28:18 +0000

    Merged revisions 11172 via svnmerge from svn+ssh://svn@code.creativecommons.org/svnroot/metadata_scraper/trunk
    ........
      r11172 | bse | 2008-11-12 01:26:44 +0000 (Wed, 12 Nov 2008) | 2 lines
      
      link to support.cc/join rather than support.cc/
    ........

--- javascript/src/campaign.js
+++ javascript/src/campaign.js
@@ -9,7 +9,7 @@ YAHOO.cc.campaign.show = function() {
     var header = YAHOO.util.Dom.get("header");
 
     // construct the text to insert
-    var network_text = '<div class="donate-today">Help Build the Commons &mdash; <a href="http://support.creativecommons.org/?utm_source=deeds&utm_medium=banner&utm_campaign=fall2008">Donate Today!</a></div> <a href="http://support.creativecommons.org/?utm_source=deeds&utm_medium=banner&utm_campaign=fall2008" id="sticker-link">&nbsp;</a> <div class="progress"><div class="progress-text">2008 Campaign</div> <div class="meter" id="meter" onclick="window.location=\'http://support.creativecommons.org?utm_source=deeds&utm_medium=banner&utm_campaign=fall2008\';"><span>&nbsp;</span></div><div class="progress-text progress-goal">$500,000</div></div>';
+    var network_text = '<div class="donate-today">Help Build the Commons &mdash; <a href="http://support.creativecommons.org/join?utm_source=deeds&utm_medium=banner&utm_campaign=fall2008">Donate Today!</a></div> <a href="http://support.creativecommons.org/join?utm_source=deeds&utm_medium=banner&utm_campaign=fall2008" id="sticker-link">&nbsp;</a> <div class="progress"><div class="progress-text">2008 Campaign</div> <div class="meter" id="meter" onclick="window.location=\'http://support.creativecommons.org/join?utm_source=deeds&utm_medium=banner&utm_campaign=fall2008\';"><span>&nbsp;</span></div><div class="progress-text progress-goal">$500,000</div></div>';
 
     // create the new module to display the alert
     var module = new YAHOO.widget.Module("campaign", {visible:false});
