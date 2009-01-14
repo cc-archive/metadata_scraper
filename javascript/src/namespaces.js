51388d3cfc89ad1cac1c50f46b1b1cf2e913a1d0
nyergler <nyergler@committer.creativecommons.org>
2009-01-14 00:15:33 +0000

    Added SIOC_SERVICE namespace object.

--- javascript/src/namespaces.js
+++ javascript/src/namespaces.js
@@ -2,6 +2,10 @@ var SIOC = function(part) {
     return "http://rdfs.org/sioc/ns#" + part;
 } // SIOC
 
+var SIOC_SERVICE = function(part) {
+    return "http://rdfs.org/sioc/services#" + part;
+} // SIOC
+
 var POWDER = function(part) {
     return "http://www.w3.org/2007/05/powder#" + part;
 } // POWDER
