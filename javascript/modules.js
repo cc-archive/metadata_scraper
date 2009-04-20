69e6c4a0099c599c1c53a0740e317200033c10db
nyergler <nyergler@committer.creativecommons.org>
2009-04-20 18:58:45 +0000

    Common module definitions.

---
+++ javascript/modules.js
@@ -0,0 +1,12 @@
+YAHOO.namespace("cc.modules");
+
+YAHOO.cc.modules.add_definitions = function(loader, basePath) {
+    // add module definitions to the loader, using basePath as the root
+
+    loader.addModule({name: "cc.ld",
+		type: "js",
+		fullpath: "http://mirrors.creativecommons.org/ld/ld.js" 
+		});
+
+} // add_definitions
+
