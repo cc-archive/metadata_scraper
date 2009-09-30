64f4623c67c4e437b97283c0ea105b16f13dde0a
nyergler <nyergler@committer.creativecommons.org>
2009-09-30 22:00:59 +0000

    Regenerated with indexOf bug fix.

--- javascript/deed-dbg.js
+++ javascript/deed-dbg.js
@@ -101,6 +101,20 @@ function addQSParameter(url, key, value) {
     return url_nohash + hash;
 } // addQSParameter
 
+YAHOO.cc.index_of = function(arr, item) {
+    // return the index of item in arr; use arr.indexOf if available
+
+    if (arr.index_of) {
+	return arr.index_of(item);
+    }
+
+    for (var i=0; i < arr.length; i++) {
+	if (arr[i] == item) return i;
+    }
+
+    return -1;
+
+} // index_of
 
 
 YAHOO.cc.license_uri = function(license_uri) {
@@ -435,7 +449,7 @@ YAHOO.cc.success = function (response) {
     var subject = null;
 
     // see if the referrer has metadata and is licensed under this license
-    if ( (metadata.subjects.indexOf(referer) > -1) &&
+    if ( (YAHOO.cc.index_of(metadata.subjects, referer) > -1) &&
          (YAHOO.cc.get_license(metadata.triples, referer) == license_url) ) {
 
 	subject = referer;
